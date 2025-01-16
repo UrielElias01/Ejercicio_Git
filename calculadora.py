c540b548b86ebe3d48ffd16e11f36694f7078db4
DanielaJanethCruz <daniela122cruz.11@gmail.com>
2025-01-15 21:49:33 -0600

    Actividad calculadora

---
+++ calculadora.py
@@ -0,0 +1,22 @@
+num1 = float(input("Introduce el primer numero: "))
+num2 = float(input("Introduce el segundo numero: "))
+
+operador = input("Introduce la operacion a realizar (suma, resta, multiplicacion, division): ")
+
+resultado = 0
+
+if operador == 'suma':
+    resultado = num1 + num2
+elif operador == 'resta':
+    resultado = num1 - num2
+elif operador == 'multiplicacion':
+    resultado = num1 * num2
+elif operador == 'division':
+    if num2 != 0:
+        resultado = num1 / num2
+    else:
+        resultado = "Error: No se puede dividir por cero"
+else:
+    resultado = "Operacion no valida"
+
+print("El resultado es", resultado)
